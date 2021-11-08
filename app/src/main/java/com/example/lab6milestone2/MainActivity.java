3842ca5a2cb8e9a06b00dc7cc954ef8cb91500cb
Katie Korth <kmkorth2@wisc.edu>
2021-11-07 22:47:03 -0600

    Initial commit

---
+++ app/src/main/java/com/example/lab6milestone2/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.lab6milestone2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends FragmentActivity {
+
+    private final LatLng mDestinationLatLng = new LatLng(43.0754097,-89.4036975);
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+            googleMap.addMarker(new MarkerOptions().position(mDestinationLatLng).title("Bascom Hall"));
+        });
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+}
